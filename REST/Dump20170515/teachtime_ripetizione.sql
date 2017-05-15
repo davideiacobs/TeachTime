d7096b6d28ee6d23b4d61273e0e3bb006ac27a09
david <davideiacobs@gmail.com>
2017-05-15 14:48:31 +0200

    dump

---
+++ REST/Dump20170515/teachtime_ripetizione.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE  IF NOT EXISTS `teachtime` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `teachtime`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: teachtime
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ripetizione`
+--
+
+DROP TABLE IF EXISTS `ripetizione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ripetizione` (
+  `ID` int(10) NOT NULL,
+  `luogo_incontro` varchar(250) DEFAULT 'a scelta dello studente',
+  `costo_per_ora` int(11) NOT NULL,
+  `descrizione` text,
+  `citta` varchar(45) NOT NULL,
+  `utente_ID` int(10) NOT NULL,
+  `materia_ID` int(10) NOT NULL,
+  PRIMARY KEY (`ID`,`materia_ID`),
+  KEY `fk_ripetizione_utente_idx` (`utente_ID`),
+  KEY `fk_ripetizione_materia1_idx` (`materia_ID`),
+  CONSTRAINT `fk_ripetizione_materia1` FOREIGN KEY (`materia_ID`) REFERENCES `mydb`.`materia` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ripetizione_utente` FOREIGN KEY (`utente_ID`) REFERENCES `mydb`.`utente` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ripetizione`
+--
+
+LOCK TABLES `ripetizione` WRITE;
+/*!40000 ALTER TABLE `ripetizione` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ripetizione` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-15 14:47:55
